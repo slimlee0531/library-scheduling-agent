3e9b8f05b7b1661663b21257d9a15bc0357f2af2
slimlee0531 <slimlee0531@163.com>
2026-05-22 20:19:45 +0800

    Initial commit: Library Scheduling Agent

---
+++ src/main/java/com/slim/agent/LibrarySchedulingAgentApplication.java
@@ -0,0 +1,13 @@
+package com.slim.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySchedulingAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarySchedulingAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/slim/agent/LibrarySchedulingAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slim.agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySchedulingAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
